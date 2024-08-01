b86d29402c7745ac19862c5e581a497d464c0559
Oleksii Haiduk <haiduko@finsfera.ua>
2024-08-02 01:38:51 +0300

    24.1. Playwright CLI and ENV variables

--- global.teardown.js
+++ global.teardown.js
@@ -1,3 +1,7 @@
-export default function (){
-    console.log("GLOBAL TEARDOWN")
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default function () {
+  console.log("GLOBAL TEARDOWN");
 }
--- playwright.config.js
+++ playwright.config.js
@@ -1,38 +1,31 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-  //testDir: './tests',
   testMatch: '/tests/**/*.spec.js',
   testIgnore: '/tests/**/*.skip.spec.js',
   globalSetup: process.env.ENV === 'stage' ? './global.setup.js' : undefined,
   globalTeardown: './global.teardown.js',
-  /* Run tests in files in parallel */
   fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Rretries: 1,
-  /* Opt out of parallel tests on CI. */
   workers: 3,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { outputFolder: 'playwright-report' }]],
   use: {
     headless: false,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://qauto.forstudy.space/',
+    baseURL: process.env.BASE_URL,
     httpCredentials: {
-      username: 'guest',
-      password: 'welcome2qauto'
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD
     },
     viewport: {
       width: 1920,
       height: 1080
     },
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
-    // video: 'on',
     screenshot: 'only-on-failure'
   },
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "setup:stage",
@@ -46,35 +39,17 @@ export default defineConfig({
       name: 'stage',
       use: {
         ...devices['Desktop Chrome'],
-        baseURL: 'https://qauto.forstudy.space/',
+        baseURL: process.env.BASE_URL,
       },
       dependencies: ['setup:stage'],
       teardown: 'teardown:stage'
     },
-
     {
       name: 'dev',
       use: {
         ...devices['Desktop Chrome'],
         baseURL: 'https://qauto2.forstudy.space/',
       },
-    }],
-  // projects: [
-  //   {
-  //     name: 'Chromium',
-  //     use: { browserName: 'chromium', headless: true, baseURL: 'https://example.com' },
-  //   },
-  //   {
-  //     name: 'Firefox',
-  //     use: { browserName: 'firefox', headless: true, baseURL: 'https://example.com' },
-  //   },
-  //   {
-  //     name: 'WebKit',
-  //     use: { browserName: 'webkit', headless: true, baseURL: 'https://example.com' },
-  //   },
-  //   {
-  //     name: 'Edge',
-  //     use: { browserName: 'chromium', headless: true, baseURL: 'https://example.com' },
-  //   },
-  // ],
+    }
+  ]
 });
